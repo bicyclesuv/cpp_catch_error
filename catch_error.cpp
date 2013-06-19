f52a810a317a26875130ed2d2e07946fca7741ab
U-EBSDEV\patrickyang <kevinyangtianjin@gmail.com>
2013-06-19 14:43:15 +0800

    cannot create a error

--- catch_error.cpp
+++ catch_error.cpp
@@ -14,10 +14,22 @@ int main()
 	 cerr<<e.what()<<"error comes\n";
 	 //      throw runtime_error("hello error hapens");
   }
+  
   string str;
   cin>>str;
   fs<<str;
   fs.close();
+
+
+  int abc= 6;
+  string str2;
+  try {
+    str2 = abc;
+  } catch (const exception ce){
+    cout<<ce.what()<<"cannot simply cast a int to a sring"<<endl;
+  }
+  cout<<str2;
+  cout<<"run to here"<<endl;
   return 0;
 }
   
